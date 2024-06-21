b6a51141c8cdbfdafb155570da8b373eca8bd047
sharjeel ali <sharjeelali6@gmail.com>
2024-06-21 18:01:22 +0500

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="heart-stroke-predictior",
+    version="0.1.1",
+    author="Sharjeel Ali",
+    author_email="sharjeelali6@gmail.com"
+    packages=find_packages(),
+    install_requires=[],
+)
